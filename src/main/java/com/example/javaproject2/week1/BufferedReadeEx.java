cf2028a74301d55683f82795bed3be1832f90b76
jyj9111 <mrjang9111@gmail.com>
2023-04-19 14:46:06 +0900

    feat: BufferedReader 추가 - 2개의 문자열 입력 받아서 문자열 그대로 출력

---
+++ src/main/java/com/example/javaproject2/week1/BufferedReadeEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReadeEx {
+    public void readALine() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);//카트가지고오기
+
+        System.out.println("line1 = " + br.readLine());
+        System.out.println("line2 = " + br.readLine());
+
+    }
+}
