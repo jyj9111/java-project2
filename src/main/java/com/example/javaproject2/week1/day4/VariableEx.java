3af3b56e1c37dbf6332661c39ffb77e919bc4f81
jyj9111 <mrjang9111@gmail.com>
2023-04-20 15:14:21 +0900

    feat: 변수선언과 초기화예제 추가 - 여러타입의 변수를 선언하는 것과 초기화하는 방법

---
+++ src/main/java/com/example/javaproject2/week1/day4/VariableEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.javaproject2.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal1;
+        PrintHello printHello;
+
+        iVal1 = 0;
+        printHello = new PrintHello();
+
+        System.out.println(iVal1);
+        printHello.print();
+    }
+}
