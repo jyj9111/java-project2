453d592ba4e535e1bdd35f7ac0dc7efbeba9f7a2
jyj9111 <mrjang9111@gmail.com>
2023-04-20 15:30:59 +0900

    feat: 변수에 값을 담아서 출력하는 예제 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1014.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char firstChar = sc.next().charAt(0);
+        char secondChar = sc.next().charAt(0);
+        System.out.printf("%s %s",secondChar,firstChar);
+    }
+}
