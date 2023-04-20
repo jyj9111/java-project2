5325b12e81d7a436411ff72ec185c7e84db4ebec
jyj9111 <mrjang9111@gmail.com>
2023-04-20 10:39:15 +0900

    feat: 숫자입력 안내문구 추가 - 입력 받을 시 안내문구 출력

---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerEx2Test.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1.day4;
+
+public class ScannerEx2Test {
+    public static void main(String[] args) {
+        ScannerEx2 sc = new ScannerEx2();
+        sc.inputTwoNumbersAndPlus();
+    }
+}
