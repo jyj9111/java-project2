3d19f0783a063cf1ef58c318249e3d6a9eee0534
jyj9111 <mrjang9111@gmail.com>
2023-04-20 11:32:56 +0900

    feat: printf를 사용한 구구단 추가 - printf()을 사용하여 구구단 출력예제

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintfTwoValuesShortage.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfTwoValuesShortage {
+    public static void main(String[] args) {
+        //System.out.printf("%s님 안녕하세요. %s까지 와주세요","장용진");
+    }
+}
