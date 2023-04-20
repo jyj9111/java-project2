8c4dad2b53d35cb9a66cc07aecd79b40dcf23299
jyj9111 <mrjang9111@gmail.com>
2023-04-20 09:23:26 +0900

    feat: [복습] Scanner를 사용해서 입력받기 - 숫자 2개 입력 받은 후 더한값을 출력

--- src/main/java/com/example/javaproject2/week1/ScannerEx.java
+++ src/main/java/com/example/javaproject2/week1/ScannerEx.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ScannerEx {
     public void readTwoNumbersAndPlus(){
         //숫자 2개 입력 받기
+        //100 + 200 = 300
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextInt() + sc.nextInt());
     }
--- src/main/java/com/example/javaproject2/week1/ScannerExTest.java
+++ src/main/java/com/example/javaproject2/week1/ScannerExTest.java
@@ -2,6 +2,7 @@ package com.example.javaproject2.week1;
 
 public class ScannerExTest {
     public static void main(String[] args) {
+        //복습
         ScannerEx se = new ScannerEx();
         se.readTwoNumbersAndPlus();
     }
