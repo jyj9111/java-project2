c36ee7fb1812fa5712e661b1801c0d72749e812f
jyj9111 <mrjang9111@gmail.com>
2023-04-21 13:23:36 +0900

    feat: Member변수 예제 추가 -멤버변수 location의 증감예제

--- src/main/java/com/example/javaproject2/week1/day5/SpaceInvader.java
+++ src/main/java/com/example/javaproject2/week1/day5/SpaceInvader.java
@@ -4,9 +4,17 @@ public class SpaceInvader {
    private void moveLeft() {
        int location = 0;
        location = -1;
+       System.out.printf("move left: %d ", location);
    }
    private void moveRight() {
        int location = 0;
        location = 1;
+       System.out.printf("move right: %d ", location);
    }
+
+    public static void main(String[] args) {
+        SpaceInvader si = new SpaceInvader();
+        si.moveLeft();
+        si.moveRight();
+    }
 }
