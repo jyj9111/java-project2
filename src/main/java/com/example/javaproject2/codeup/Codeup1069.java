98e06b566941f4fc2204a5e7c11a1c6be768712e
jyj9111 <mrjang9111@gmail.com>
2023-04-28 13:20:50 +0900

    feat: Codeup 1069-1070 추가 - 코드업문제 swtich,case 예제 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1069.java
@@ -0,0 +1,28 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1069 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String grade = sc.nextLine();
+
+        switch (grade) {
+            case "A":
+                System.out.println("best!!!");
+                break;
+            case "B":
+                System.out.println("good!!");
+                break;
+            case "C":
+                System.out.println("run!");
+                break;
+            case "D":
+                System.out.println("slowly~");
+                break;
+            default:
+                System.out.println("what?");
+        }
+    }
+}
