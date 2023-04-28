dd12a1f8cd2bc92ad721c9cbce345310c42aaeb2
jyj9111 <mrjang9111@gmail.com>
2023-04-28 09:18:58 +0900

    feat: if,else 제어문 예제 추가 - IfEx, IfComparisonOperation, ElseEx, ElseEx2 추가

---
+++ src/main/java/com/example/javaproject2/week2/day5/IfEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day5;
+
+public class IfEx {
+    public static void main(String[] args) {
+
+        if(true){ //조건이 true 이면 실행
+            System.out.println("항상 실행 됩니다.");
+        }
+        if(false) { // 조건이 false 이면 실행되지 않음
+            System.out.println("항상 실행 되지 않습니다.");
+        }
+    }
+}
