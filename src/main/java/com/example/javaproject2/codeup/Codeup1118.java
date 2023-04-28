19f7a067e0561d629c45a48d60e074fa9e564f59
jyj9111 <mrjang9111@gmail.com>
2023-04-28 14:12:57 +0900

    feat: Codeup 1116-1118 추가 -

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1118.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1118 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int base = sc.nextInt();
+        int height = sc.nextInt();
+
+        System.out.printf("%.1f\n", base * height * 0.5);
+    }
+}
