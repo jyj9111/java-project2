4c0f179ed2b7137bcb79fae8e503b2e06040d7c5
jyj9111 <mrjang9111@gmail.com>
2023-04-28 10:27:36 +0900

    feat: else if 제어문 예제 추가 - IfWrong, GetGrade 추가

---
+++ src/main/java/com/example/javaproject2/week2/day5/GetGrade.java
@@ -0,0 +1,27 @@
+package com.example.javaproject2.week2.day5;
+
+public class GetGrade {
+    public static void main(String[] args) {
+        int score = 75;
+
+        if(score >= 90) {
+            System.out.println("A");
+        } else if(score >= 80) {
+            System.out.println("B");
+        } else if(score >= 70) {
+            System.out.println("C");
+        } else {
+            System.out.println("F");
+        }
+        // 잘못된 예
+        /*if(score >= 70) {
+            System.out.println("C");
+        } else if(score >= 80) {
+            System.out.println("B");
+        } else if(score >= 90) {
+            System.out.println("A");
+        } else {
+            System.out.println("F");
+        }*/
+    }
+}
