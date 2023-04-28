cf8772d6a741e3ea37eb6655bacc5ae6f12db35c
jyj9111 <mrjang9111@gmail.com>
2023-04-28 09:43:18 +0900

    feat: CodeUp 1065-1067 추가 -코드업 if문 문제 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1067.java
@@ -0,0 +1,29 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1067 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+
+        if (num1 < 0) {
+            System.out.println("minus");
+
+            if (num1 % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        } else {
+            System.out.println("plus");
+
+            if (num1 % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+    }
+}
