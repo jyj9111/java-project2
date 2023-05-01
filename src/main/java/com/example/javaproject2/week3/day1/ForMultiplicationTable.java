937ef6440c5f5098f74bc2c0a0109c89a21f16ce
jyj9111 <mrjang9111@gmail.com>
2023-05-01 14:36:52 +0900

    fix: 중첩 반복문 사용 (2~4단) - 중첩 반복문사용

--- src/main/java/com/example/javaproject2/week3/day1/ForMultiplicationTable.java
+++ src/main/java/com/example/javaproject2/week3/day1/ForMultiplicationTable.java
@@ -2,20 +2,12 @@ package com.example.javaproject2.week3.day1;
 
 public class ForMultiplicationTable {
     public static void main(String[] args) {
-        // 구구단 2단
-        for (int i = 1; i <= 9 ; i++) {
-            System.out.printf("2 * %d = %d\n", i, i * 2);
+        // 구구단 2단 ~ 4단
+        for (int i = 2; i <= 4 ; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%d * %d = %d\n", i, j, j * i);
+            }
+            System.out.println("=============");
         }
-        System.out.println("============");
-        // 구구단 3단
-        for (int i = 1; i <= 9 ; i++) {
-            System.out.printf("3 * %d = %d\n", i, i * 3);
-        }
-        System.out.println("============");
-        // 구구단 4단
-        for (int i = 1; i <= 9 ; i++) {
-            System.out.printf("4 * %d = %d\n", i, i * 4);
-        }
-        System.out.println("============");
     }
 }
