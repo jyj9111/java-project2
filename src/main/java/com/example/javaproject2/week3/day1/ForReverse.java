4a3f46a3e0e8be9249907b929f0ef4fe78be568b
jyj9111 <mrjang9111@gmail.com>
2023-05-01 11:29:55 +0900

    feat: ForReverse 추가 - for반복문 Reverse 예제 - 자주틀리는 케이스 추가

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForReverse.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForReverse {
+    public static void main(String[] args) {
+        // 자주 틀리는 케이스 , 증감 연산자 실수
+        /*for (int i = 10; i > 0 ; i++) {
+            System.out.println(i);
+        }*/
+        // 5 ~ 1 까지 출력
+        /*for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }*/
+        // 5 ~ 0 까지 출력
+        for (int i = 5; i >= 0 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
