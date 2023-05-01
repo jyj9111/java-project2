094deac7becea238d0ac3b7e87325cd328552a9d
jyj9111 <mrjang9111@gmail.com>
2023-05-01 11:46:15 +0900

    feat: Codeup 1073-1075 추가 -

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1073.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1073 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; num != 0; i++) {
+            System.out.println(num);
+            num = sc.nextInt();
+        }
+    }
+}
