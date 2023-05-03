f83edb643c6bd2678fe3a5691449071acf0beb71
jyj9111 <mrjang9111@gmail.com>
2023-05-03 13:09:57 +0900

    feat: Codeup 1274 추가 -

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1274.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1274 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int fCount = 0;
+
+        for (int i = 2; i < num; i++) {
+            if(num % i == 0) fCount++;
+        }
+        if(fCount == 0) System.out.println("prime");
+        else System.out.println("not prime");
+    }
+}
