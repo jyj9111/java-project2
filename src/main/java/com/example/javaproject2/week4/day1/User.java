909640d40bac4b747c0eeaa1d52e9e7f6be6a843
jyj9111 <mrjang9111@gmail.com>
2023-05-11 10:28:34 +0900

    fix: User Obj에 Constructor, Getter, Setter 추가 - 4week-4day

--- src/main/java/com/example/javaproject2/week4/day1/User.java
+++ src/main/java/com/example/javaproject2/week4/day1/User.java
@@ -1,9 +1,41 @@
 package com.example.javaproject2.week4.day1;
 
 public class User {
-    public String name;
-    public String phoneNumber; //맨 앞 0이짤림
-    public int age;
+    private String name;
+    private String phoneNumber; //맨 앞 0이짤림
+    private int age;
+
+    public User(String name, String phoneNumber, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     boolean isAdult() {
         return age >= 18;
