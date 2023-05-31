7adfca663d2c7f1f2c0e0d10c4a62ab207322e57
jyj9111 <mrjang9111@gmail.com>
2023-05-31 18:39:17 +0900

    feat: jdbc를 사용한 db연결 및 사용 예제 추가 - Refactoring을 거친 코드 추가 - 7week-3day

---
+++ src/main/java/com/example/javaproject2/week7/day3/model/Person.java
@@ -0,0 +1,24 @@
+package com.example.javaproject2.week7.day3.model;
+
+public class Person {
+    int id;
+    String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%,03d %s", id, name);
+    }
+}
